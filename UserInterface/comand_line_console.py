0ecb42a7eee96e9ca47d1ebe8ead9dfa399b9805
baracdavid <david_barucu10@yahoo.com>
2021-11-03 14:27:08 +0200

    a adaugat fiecare functionalitate

--- UserInterface/comand_line_console.py
+++ UserInterface/comand_line_console.py
@@ -1,14 +1,16 @@
 
 from Domain.vanzare2 import get_string, create_vanzare
 from Logic.crud import create, delete, update
+from Logic.modificare_gen import modificare_gen_dupa_titlu
+from Logic.reducere_pret import reducere_pret
 
 
 def command_line_console(vanzari):
     # add 1, Name, Gen, Price, Type
-    command_line_str = input('Introduceti comanda(separator comenzi ";" iar separator paraetri ","): ')
     print('''
-        exemple de comenzi:add,1,2,4,5,none;delete,1;update,1,222,422,225,silver;showall
-    ''')
+           exemple de comenzi: add,1,Titlu_1,Gen_1,5,none;add,3,Titlu_3,Gen_3,5,none;add,20,Titlu_20,Gen_20,5,none;update,1,Titlu_1_nou,Gen_1_nou,225,silver;reducere;modifica_gen,Titlu_3,Gen_3_mod;showall
+       ''')
+    command_line_str = input('Introduceti comanda(separator comenzi ";" iar separator paraetri ","): ')
 
 
 
@@ -35,6 +37,17 @@ def command_line_console(vanzari):
                                  command[3], float(command[4]), command[5])
 
                 vanzari = update(vanzari, vanzare)
+            except ValueError as ve:
+                print('Eroare! Detalii: ', ve)
+        elif command[0] == 'reducere':
+            try:
+                vanzari =reducere_pret(vanzari)
+
+            except ValueError as ve:
+                print('Eroare! Detalii: ', ve)
+        elif command[0] == 'modifica_gen':
+            try:
+                vanzari = modificare_gen_dupa_titlu(vanzari, command[1], command[2])
             except ValueError as ve:
                 print('Eroare! Detalii: ', ve)
     return vanzari
